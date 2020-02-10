93155650284d4ced05f3d0c8694ae13dba9ff00f
Iurie Tarlev <it119@ic.ac.uk>
2020-02-10 17:54:56 +0000

    first commit

--- src/utils/MinPriorityQueue.java
+++ src/utils/MinPriorityQueue.java
@@ -1,43 +1,53 @@
 package utils;
 
 public class MinPriorityQueue<T extends Comparable<T>> {
-    
-    /**
-     * Creates an empty queue.
-     */
-    public MinPriorityQueue() {
-        // TODO implement the constructor
-    }
 
-    /**
-     * Returns the number of elements currently in the queue.
-     */
-    public int size() {
-        // TODO implement this method
-        return 0;
-    }
-    
-    /**
-     * Adds elem to the queue.
-     */
-    public void add(T elem) {
-        // TODO implement this method
-    }
+  /**
+   * Class variables
+   **/
+  public T queue[];
 
-    /**
-     * Removes, and returns, the element at the front of the queue.
-     */
-    public T remove() {
-        // TODO implement this method
-        return null;
-    }
+  /**
+   * Creates an empty queue.
+   */
+  public MinPriorityQueue() {
+    // TODO implement the constructor
+    queue = new T[3];
+  }
 
-    /**
-     * Returns true if the queue is empty, false otherwise.
-     */
-    public boolean isEmpty() {
-        // TODO implement this method
-        return false;
+  /**
+   * Returns the number of elements currently in the queue.
+   */
+  public int size() {
+    return queue.length;
+  }
+
+  /**
+   * Adds elem to the queue.
+   */
+  public void add(T elem) {
+    // TODO implement this method
+    if (queue.length == 0) {
+      queue[1] = elem;
+    } else {
+      queue[size()] = elem;
     }
-    
+  }
+
+  /**
+   * Removes, and returns, the element at the front of the queue.
+   */
+  public T remove() {
+    // TODO implement this method
+    return null;
+  }
+
+  /**
+   * Returns true if the queue is empty, false otherwise.
+   */
+  public boolean isEmpty() {
+    if (queue.length == 0)
+      return true;
+    return false;
+  }
 }

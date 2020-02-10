ff9f6666706a09003d1dd89cc55ef2fcd9363608
Pablo Navajas Helguero <P.Navajas-Helguero@warwick.ac.uk>
2020-02-10 21:14:47 +0100

    Updated AbstractEvent, modified Collisions and created Tick classes

--- src/simulation/Collision.java
+++ src/simulation/Collision.java
@@ -1,28 +1,59 @@
 package simulation;
 
 public abstract class Collision extends AbstractEvent{
-    
+
+    // Hey Iura and Dave! I have added the comments just so you know what I modified
+    // (no internal variables given, created by Pablo)
+    private final Particle[] particles;
+
+    // Internal variables to keep track of nº of collisions upon creation
+    private final int countA;
+    private final int countB;
+
     /**
      * Constructor for Collision
      */
     public Collision(double t, Particle[] ps) {
-        // TODO implement constructor
+        // (it was empty, implemented by Pablo)
+
+        super(t);
+        this.particles = ps;
+
+        // Initialise variables to keep track of nº of collisions upon creation
+        countA = ps[0].collisions();
+        // There can only be one particle involved in the collision
+        if (ps[1] != null) {
+            countB = ps[1].collisions();
+        }
+        else {
+            countB = -1;
+        }
     }
 
     /**
      * Returns true if this Collision is (still) valid.
+     * inspired by :
+     * https://github.com/fracpete/princeton-java-algorithms/blob/master/src/main/java/edu/princeton/cs/algorithms/CollisionSystem.java
      */
     @Override
     public boolean isValid() {
-        // TODO implement his method
-        return false;
+        // (it was empty, implemented by Pablo)
+        // Check if there have been any collisions after creation of event
+
+        if (particles[0].collisions() != countA) {
+            return false;
+        }
+        if (particles[1] != null && particles[1].collisions() != countB) {
+            return false;
+        }
+        return true;
     }
 
     /**
      * Returns an array containing the Particles involved in this Collision.
      */
     public Particle[] getParticles() {
-        // TODO implement this method
-        return null;
+        // (it was empty, implemented by Pablo)
+        return particles;
     }
 }
---
+++ src/simulation/Tick.java
@@ -0,0 +1,25 @@
+package simulation;
+
+public class Tick extends AbstractEvent {
+  // Fully created this class
+
+  /**
+   * Constructor for Tick
+   */
+  public Tick(double t) {
+    // (it was empty, implemented by Pablo)
+    super(t);
+  }
+
+  @Override
+  public void happen(ParticleEventHandler h) {
+    // As per spec: "the only thing it must do is make a callback to the ParticleEventHandler"
+    h.reactTo(this);
+  }
+
+  @Override
+  public boolean isValid() {
+    // As per the spec: "a Tick is always valid"
+    return true;
+  }
+}

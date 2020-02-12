b29312eecb94f4e084778a91af978d36d5b42437
dh1416 <dh1416@ic.ac.uk>
2020-02-12 18:26:28 +0000

    Problematic comment resolved

--- src/simulation/Collision.java
+++ src/simulation/Collision.java
@@ -2,11 +2,8 @@ package simulation;
 
 public abstract class Collision extends AbstractEvent {
 
-  // Hey Iura and Dave! I have added the comments just so you know what I modified
-  // (no internal variables given, created by Pablo)
   private final Particle[] particles;
 
-  // Internal variables to keep track of nº of collisions upon creation
   private final int countA;
   private final int countB;
 
@@ -15,15 +12,12 @@ public abstract class Collision extends AbstractEvent {
    * Note: an array of maximum of two particles should be passed in
    */
   public Collision(double t, Particle[] ps) {
-    // (it was empty, implemented by Pablo)
 
     super(t);
     this.particles = ps;
 
-    // Initialise variables to keep track of nº of collisions upon creation
     countA = ps[0].collisions();
 
-    // There can only be one particle involved in the collision
     if ((ps.length > 1)){
       countB = ps[1].collisions();
     } else {
@@ -38,8 +32,6 @@ public abstract class Collision extends AbstractEvent {
    */
   @Override
   public boolean isValid() {
-    // (it was empty, implemented by Pablo)
-    // Check if there have been any collisions after creation of event
     if (particles[0].collisions() != countA) {
       return false;
     }
@@ -53,7 +45,6 @@ public abstract class Collision extends AbstractEvent {
    * Returns an array containing the Particles involved in this Collision.
    */
   public Particle[] getParticles() {
-    // (it was empty, implemented by Pablo)
     return particles;
   }
 }
